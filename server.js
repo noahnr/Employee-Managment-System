fd195fd8f62df4097209d13b39906ae9b06935b5
Noah Soukup <noahsoukupraesch@gmail.com>
2020-07-10 16:28:13 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "top_songsDB"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "top_songsDB"
+});
