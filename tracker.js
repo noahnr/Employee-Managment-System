d03b0d7b0c8a2b47971273d2b8d001723ba0f3ba
Noah Soukup <noahsoukupraesch@gmail.com>
2020-07-10 16:45:50 -0500

    additions

--- tracker.js
+++ tracker.js
@@ -1,14 +1,85 @@
-var inquirer = require("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+// const createTable = require("console.table");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "top_songsDB"
+const connection = mysql.createConnection({
     host: "localhost",
+
+    // Your port; if not 3306
     port: 3306,
+
+    // Your username
     user: "root",
-    password: "password",
-    database: "top_songsDB"
+
+    // Your password
+    password: "Rootbeer1",
+    database: "employTrack_db"
 });
+
+connection.connect((err) => {
+    if (err) throw err;
+    beginPrompt();
+});
+let beginPrompt = () => {
+  inquirer
+      .prompt({
+          name: "action",
+          type: "list",
+          message: "what would you like to do",
+          choices: [
+              "View all employees",
+              "View all departments",
+              "View all employees by manager id",
+              "Add employee",
+              "Remove employee",
+              "Remove manager",
+              "View all roles",
+              "Add role",
+              "Remove role",
+              "Exit"
+          ]
+      }) // begin switch statements for prompted answers
+      .then((answer) => {
+          switch (answer.action) {
+              case "View all employees":
+                  viewEmployees();
+                  break;
+
+              case "View all departments":
+                  viewEmployeesDept();
+                  break;
+
+              case "View all employees by manager id":
+                  viewEmployeesManager();
+                  break;
+
+              case "Add employee":
+                  addEmployee();
+                  break;
+
+              case "Remove employee":
+                  removeEmployee();
+                  break;
+
+              case "Remove manager":
+                  removeManager();
+                  break;
+
+              case "View all roles":
+                  viewRoles();
+                  break;
+
+              case "Add role":
+                  addRole();
+                  break;
+
+              case "Remove role":
+                  removeRole();
+                  break;
+
+              case "Exit":
+                  connection.end();
+                  break;
+          }
+      });
+}
