c95873dc1273ebd4003fdc2103cc7c4523215e01
Noah Soukup <noahsoukupraesch@gmail.com>
2020-07-11 10:12:24 -0500

    real sql, mistake, I codded it in vs code and copied the wrong thing

--- Employee_Database.sql
+++ Employee_Database.sql
@@ -1,63 +1,30 @@
-let removeEmployee = () => {
-  connection.query("SELECT * FROM employee", (err, results) => {
-      if (err) throw err;
-      inquirer
-          .prompt([{
-              name: "remove",
-              type: "input",
-              message: "Enter employee ID# you wish to remove"
-          }])
-          .then((answer) => {
-              connection.query("DELETE FROM employee where ?", {
-                  id: answer.remove
-
-              });
-              console.log("Successfully deleted employee")
-              beginPrompt();
-          });
-  });
-
-}
-
-let removeRole = () => {
-  connection.query("SELECT * FROM emprole", (err, results) => {
-      if (err) throw err;
-      inquirer
-          .prompt([{
-              name: "remove",
-              type: "input",
-              message: "Enter role ID# you wish to remove"
-          }])
-          .then((answer) => {
-              connection.query("DELETE FROM emprole where ?", {
-                  id: answer.remove
-
-              });
-              console.log("Successfully deleted role")
-              beginPrompt();
-          });
-  });
-
-}
-
-let removeManager = () => {
-  connection.query("SELECT * FROM employee", (err, results) => {
-      if (err) throw err;
-      inquirer
-          .prompt([{
-              name: "remManager",
-              type: "input",
-              message: "Enter manager ID# you wish to remove (5, 6, 7)"
-          }])
-          .then((answer) => {
-              connection.query("DELETE FROM employee where ?", {
-                 manager_id: answer.remManager
-
-              });
-              console.log("Successfully deleted manager");
-              console.table(answer);
-              beginPrompt();
-          });
-  });
-
-}
+DROP DATABASE IF EXISTS employTrack_db;
+Create database employTrack_db;
+USE employTrack_db;
+   
+    --Department Table
+CREATE TABLE department (
+	id INTEGER NOT NULL,
+	deptName VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+    --empRole Table
+CREATE TABLE emprole (
+	id INT NOT NULL,
+	title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+ --Employee table
+CREATE TABLE employee (
+	id INT NOT NULL,
+	first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES emprole(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id) 
+);
